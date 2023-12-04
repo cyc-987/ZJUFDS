0c4aea0ea64a8588e2f80c06bba98e05e49dc25a
cyc-987 <2516803593@qq.com>
2023-12-04 18:54:14 +0800

    题目测试点通过

--- Project3/DijkstraSequence.c
+++ Project3/DijkstraSequence.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define maxV 1001
+#define infinity 100001
 
 //node of adlist
+typedef struct Node* ptrtoNode;
 typedef struct Node{
     int ver;
     int weight;
     ptrtoNode Next;
 }Node;
-typedef Node* ptrtoNode;
+
 //a list of ver
 typedef struct Ver{
     ptrtoNode FirstEdge;
@@ -22,7 +24,8 @@ typedef struct Graph{
 typedef Graph* ptrG;
 
 //some functions
-void addEdge(ptrG G, int first, int second, int weight);
+void addEdge(ptrG G, int first, int second, int weight);//add edges
+int findMin(int Known[],long int Dist[],long int Nv,long int startPoint);//find the min distance in unknown vertix
 
 int main()
 {
@@ -48,18 +51,58 @@ int main()
     {
         //read sequence into an array
         int Seq[maxV] = {0};
-        for(int j = 0;j<k;j++) scanf("%d",&Seq[j+1]);
+        for(int j = 0;j<=k;j++) scanf("%d",&Seq[j+1]);
         getchar();
 
         //start inquiry
         int Known[maxV] = {0};//set known array
         long int Dist[maxV];//set distance array
-        for(int j = 1;j<=Nv;j++) Dist[j] = 100000;//set to infinity
+        for(int j = 1;j<=Nv;j++) Dist[j] = infinity;//set to infinity
         //do not need p[]
 
         //process first node
-        Known[Seq[1]] = 1;
+        int flag = 1;//record the result
         Dist[Seq[1]] = 0;
+
+        //main part of dijkstra
+        int count = 1;
+        for(;;)
+        {
+            long int v,w;
+            if(!(v = findMin(Known,Dist,Nv,0))) break;//end situation
+            
+            //check element
+            while(v){
+                if(v != Seq[count]) v = findMin(Known,Dist,Nv,v);
+                else break;
+            }
+            if(v == 0){
+                flag = 0;
+                break;
+            }else{
+                count++;
+            }
+
+            Known[v] = 1;
+            ptrtoNode temp = G->L[v].FirstEdge;
+            while(temp){
+                if(!Known[temp->ver]){
+                    if(Dist[v]+temp->weight < Dist[temp->ver])
+                    {
+                        //update w
+                        Dist[temp->ver] = Dist[v]+temp->weight;
+                    }
+                }
+                temp = temp->Next;
+            }
+        }
+
+        //print result
+        if(flag){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
     }
 }
 
@@ -74,10 +117,44 @@ void addEdge(ptrG G, int first, int second, int weight)
         G->L[first].FirstEdge = temp;
     }else{
         ptrtoNode temp = G->L[first].FirstEdge;
-        while(temp) temp = temp->Next;
-        temp = (ptrtoNode)malloc(sizeof(Node));
+        while(temp->Next) temp = temp->Next;
+        temp->Next = (ptrtoNode)malloc(sizeof(Node));
+        temp = temp->Next;
         temp->Next = NULL;
         temp->ver = second;
         temp->weight = weight;
     }
+
+    //exchange
+    if(G->L[second].FirstEdge == NULL){
+        ptrtoNode temp = (ptrtoNode)malloc(sizeof(Node));
+        temp->Next = NULL;
+        temp->ver = first;
+        temp->weight = weight;
+        G->L[second].FirstEdge = temp;
+    }else{
+        ptrtoNode temp = G->L[second].FirstEdge;
+        while(temp->Next) temp = temp->Next;
+        temp->Next = (ptrtoNode)malloc(sizeof(Node));
+        temp = temp->Next;
+        temp->Next = NULL;
+        temp->ver = first;
+        temp->weight = weight;
+    }
+}
+
+int findMin(int Known[],long int Dist[],long int Nv,long int startPoint)//find the min distance in unknown vertix
+{
+    int i = startPoint+1;
+    long int min = 0;
+    for(;i<=Nv;i++){
+        if(Known[i] == 1)continue;
+
+        if(min == 0){
+            min = i;
+        }else{
+            if(Dist[min] > Dist[i]) min = i;
+        }
+    }
+    return min;
 }
