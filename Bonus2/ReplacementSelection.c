c605d346be2f8d13f6b9bc8cfa852e6cc3559486
cyc-987 <2516803593@qq.com>
2023-12-19 16:45:00 +0800

    改进版本，时间复杂度仍然过高

--- Bonus2/ReplacementSelection.c
+++ Bonus2/ReplacementSelection.c
@@ -3,6 +3,7 @@
 #define MaxN 100
 
 void sortMin(int list[], long long N);
+void maintainSort(int list[], long long N, long long index);
 void deleteIndex(int list[], long long *N, long long index);
 
 int main()
@@ -23,9 +24,9 @@ int main()
     int lastnumber = -32766;
     int ram[maxRam];
     for(i=0;i<maxRam;i++) ram[i] = list[readCount++];//read first list of numbers
+    sortMin(ram,maxRam);
 
     while(maxRam){
-        sortMin(ram,maxRam);//find the min in ram
         long long index = 0;
         while(index < maxRam && ram[index] < lastnumber) index++;
         if(index == maxRam){//start a new line and reset
@@ -41,6 +42,7 @@ int main()
         lastnumber = ram[index];//update lastnumber
         if(readCount < totalNum){
             ram[index] = list[readCount++];
+            maintainSort(ram,maxRam,index);
             continue;
         }else{
             deleteIndex(ram,&maxRam,index);
@@ -68,4 +70,33 @@ void deleteIndex(int list[], long long *N, long long index)
     long long i = index;
     for(;i<*N-1;i++) list[i] = list[i+1];
     (*N)--;
+}
+
+void maintainSort(int list[], long long N, long long index)
+{
+    if(index == -1) return;
+
+    int flag;
+    if(index == N) flag = 1;
+    else if(index == 0) flag = 2;
+    else if(list[index-1] > list[index]) flag = 1;
+    else if(list[index+1] < list[index]) flag = 2;
+
+    if(flag == 1){
+        int temp = list[index];
+        while(index > 0){
+            if(list[index-1] > temp) list[index] = list[index-1];
+            else break;
+            index--;
+        }
+        list[index] = temp;
+    }else if(flag == 2){
+        int temp = list[index];
+        while(index<N-1){
+            if(list[index+1] < temp) list[index] = list[index+1];
+            else break;
+            index++;
+        }
+        list[index] = temp;
+    }
 }
